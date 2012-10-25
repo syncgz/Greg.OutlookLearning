b5ece9d4fb20c748960c93075af125d2b12c6e60
syncgz <sync.gz@gmail.com>
2012-10-25 08:22:28 +0200

    Commit.

---
+++ Greg.OutlookLearning/Ribbon1.Designer.cs
@@ -0,0 +1,97 @@
+﻿namespace Greg.OutlookLearning
+{
+    partial class Ribbon1 : Microsoft.Office.Tools.Ribbon.RibbonBase
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        public Ribbon1()
+            : base(Globals.Factory.GetRibbonFactory())
+        {
+            InitializeComponent();
+        }
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tab2 = this.Factory.CreateRibbonTab();
+            this.group2 = this.Factory.CreateRibbonGroup();
+            this.button1 = this.Factory.CreateRibbonButton();
+            this.label1 = this.Factory.CreateRibbonLabel();
+            this.tab2.SuspendLayout();
+            this.group2.SuspendLayout();
+            // 
+            // tab2
+            // 
+            this.tab2.Groups.Add(this.group2);
+            this.tab2.Label = "Tab label";
+            this.tab2.Name = "tab2";
+            // 
+            // group2
+            // 
+            this.group2.Items.Add(this.button1);
+            this.group2.Items.Add(this.label1);
+            this.group2.Label = "group2";
+            this.group2.Name = "group2";
+            // 
+            // button1
+            // 
+            this.button1.Label = "button1";
+            this.button1.Name = "button1";
+            this.button1.Click += new Microsoft.Office.Tools.Ribbon.RibbonControlEventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.Label = "label1";
+            this.label1.Name = "label1";
+            // 
+            // Ribbon1
+            // 
+            this.Name = "Ribbon1";
+            this.RibbonType = "Microsoft.Outlook.Explorer";
+            this.Tabs.Add(this.tab2);
+            this.Load += new Microsoft.Office.Tools.Ribbon.RibbonUIEventHandler(this.Ribbon1_Load);
+            this.tab2.ResumeLayout(false);
+            this.tab2.PerformLayout();
+            this.group2.ResumeLayout(false);
+            this.group2.PerformLayout();
+
+        }
+
+        #endregion
+
+        private Microsoft.Office.Tools.Ribbon.RibbonTab tab2;
+        internal Microsoft.Office.Tools.Ribbon.RibbonGroup group2;
+        internal Microsoft.Office.Tools.Ribbon.RibbonButton button1;
+        internal Microsoft.Office.Tools.Ribbon.RibbonLabel label1;
+
+    }
+
+    partial class ThisRibbonCollection
+    {
+        internal Ribbon1 Ribbon1
+        {
+            get { return this.GetRibbon<Ribbon1>(); }
+        }
+    }
+}
--- Greg.OutlookLearning/ThisAddIn.cs
+++ Greg.OutlookLearning/ThisAddIn.cs
@@ -10,12 +10,38 @@ namespace Greg.OutlookLearning
 {
     public partial class ThisAddIn
     {
+        private UserControl1 myUserControl1;
+        private Microsoft.Office.Tools.CustomTaskPane myCustomTaskPane;
+
+        private Outlook.Inspectors inspectors;
+
         private void ThisAddIn_Startup(object sender, System.EventArgs e)
         {
+            myUserControl1 = new UserControl1();
+            myCustomTaskPane = this.CustomTaskPanes.Add(myUserControl1, "My Task Pane");
+            myCustomTaskPane.Visible = true;
+
+            inspectors = this.Application.Inspectors;
+            inspectors.NewInspector +=
+            new Microsoft.Office.Interop.Outlook.InspectorsEvents_NewInspectorEventHandler(Inspectors_NewInspector);
         }
 
         private void ThisAddIn_Shutdown(object sender, System.EventArgs e)
         {
+        }
+
+        void Inspectors_NewInspector(Microsoft.Office.Interop.Outlook.Inspector Inspector)
+        {
+            Outlook.MailItem mailItem = Inspector.CurrentItem as Outlook.MailItem;
+            if (mailItem != null)
+            {
+                if (mailItem.EntryID == null)
+                {
+                    mailItem.Subject = "This text was added by using code";
+                    mailItem.Body = "This text was added by using code";
+                }
+
+            }
         }
 
         #region VSTO generated code
